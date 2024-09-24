1254754eeada156ef130264ac00024495273ae48
Shahzod <sibragimov999@gmail.com>
2024-09-24 16:10:11 +0500

    update

--- frontend/src/pages/User/Profile.tsx
+++ frontend/src/pages/User/Profile.tsx
@@ -4,17 +4,15 @@ import { selectedUserInfo } from "../../app/features/userInfoSlice";
 import { useAppSelector } from "../../app/hooks";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import { Button, FormControl, Input, InputAdornment, InputLabel, Paper, Typography } from "@mui/material";
+import { Button, FormControl, FormHelperText, Input, InputAdornment, InputLabel, Paper, Typography, colors } from "@mui/material";
 import { useState } from "react";
 import { Inputs } from "../../app/types/formTypes";
 
 const Profile = () => {
-  // уважаемые Belcor) я хотел внедрить компонент auth сюда, но так легко будет потерять чиатбельность кода
-
   const inputLabelStyles = {
     fontWeight: 600,
-    "&.Mui-focused": { color: "white" }, // Цвет при фокусировке
-    "&.MuiInputLabel-root.Mui-focused": { color: "white" }, // Цвет при взаимодействии
+    "&.Mui-focused": { color: "white" },
+    "&.MuiInputLabel-root.Mui-focused": { color: "white" },
   };
 
   const userInfo = useAppSelector(selectedUserInfo);
@@ -27,36 +25,58 @@ const Profile = () => {
     watch,
     formState: { errors },
   } = useForm();
+  const password = watch("password");
 
-  const onSubmit = async (data: Inputs) => {
+  const onSubmit = async (data: any) => {
     console.log(data);
+    try {
+      const res = await updateUserProfile({ _id: userInfo?._id, ...data });
+      console.log(res);
+    } catch (error) {
+      alert(error);
+    }
   };
+
+  console.log(errors);
 
   return (
     <Paper elevation={0} sx={{ mt: 10, padding: 2, backgroundColor: "darkslategray" }} variant="elevation">
       <Typography variant="h4">Profile</Typography>
       <Typography sx={{ marginBottom: 2 }}>Would you like to change the information?</Typography>
 
-      <form >
-        <FormControl required fullWidth margin="normal">
-          <InputLabel htmlFor="userName" sx={inputLabelStyles}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl fullWidth margin="normal">
+          <InputLabel htmlFor="username" sx={inputLabelStyles}>
             User Name
           </InputLabel>
-          <Input name="userName" type="text" disableUnderline={true} />
+          <Input {...register("username", { required: "Please enter Name" })} type="text" disableUnderline={true} />
+          {errors.username && <FormHelperText sx={{ color: "#d15e5e" }}>{errors.username.message}</FormHelperText>}
         </FormControl>
 
-        <FormControl required fullWidth margin="normal">
+        <FormControl fullWidth margin="normal">
           <InputLabel htmlFor="email" sx={inputLabelStyles}>
             e-mail
           </InputLabel>
-          <Input name="email" type="email" autoComplete="email" disableUnderline={true} />
+          <Input
+            {...register("email", {
+              required: "Please enter Email",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Invalid email address",
+              },
+            })}
+            type="email"
+            autoComplete="email"
+            disableUnderline={true}
+          />
+          {errors.email && <FormHelperText sx={{ color: "#d15e5e" }}>{errors.email.message}</FormHelperText>}
         </FormControl>
-        <FormControl required fullWidth margin="normal">
-          <InputLabel htmlFor="email" sx={inputLabelStyles}>
+        <FormControl fullWidth margin="normal">
+          <InputLabel htmlFor="password" sx={inputLabelStyles}>
             password
           </InputLabel>
           <Input
-            name="password"
+            {...register("password", { required: "Please enter Password", minLength: { value: 6, message: "Password must be at least 6 characters" } })}
             type={showPassword ? "password" : "input"}
             autoComplete="password"
             disableUnderline={true}
@@ -72,15 +92,16 @@ const Profile = () => {
               )
             }
           />
+          {errors.password && <FormHelperText sx={{ color: "#d15e5e" }}>{errors.password.message}</FormHelperText>}
         </FormControl>
-        <FormControl required fullWidth margin="normal">
-          <InputLabel htmlFor="email" sx={inputLabelStyles}>
+        <FormControl fullWidth margin="normal">
+          <InputLabel htmlFor="confirmPassword" sx={inputLabelStyles}>
             Confirm Password
           </InputLabel>
           <Input
-            name="password"
+            {...register("confirmPassword", { required: "Please enter confirm Password", validate: (v) => v === password || "Passwords do not match" })}
             type={showPassword ? "password" : "input"}
-            autoComplete="password"
+            autoComplete="confirmPassword"
             disableUnderline={true}
             endAdornment={
               showPassword ? (
@@ -94,8 +115,8 @@ const Profile = () => {
               )
             }
           />
+          {errors.confirmPassword && <FormHelperText sx={{ color: "#d15e5e" }}> {errors.confirmPassword.message}</FormHelperText>}
         </FormControl>
-
         <Button disabled={false} disableRipple fullWidth variant="outlined" type="submit" sx={{ mt: 4, color: "white", borderColor: "white" }}>
           Change
         </Button>
