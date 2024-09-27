efc65ebb047ccb45f888b89b0e9cec3babb25030
Shahzod <sibragimov999@gmail.com>
2024-09-27 19:32:58 +0500

    responsive

--- frontend/src/components/filterProducts/Filter.tsx
+++ frontend/src/components/filterProducts/Filter.tsx
@@ -15,73 +15,71 @@ interface FilterProps {
   onPageFilterChange: (type: string) => (event: SelectChangeEvent<string>) => void;
 }
 
-const Filter: React.FC<FilterProps> = ({
-  categoryList,
-  filters,
-  pageFilter,
-  onFilterChange,
-  onPageFilterChange,
-}) => {
+const Filter: React.FC<FilterProps> = ({ categoryList, filters, pageFilter, onFilterChange, onPageFilterChange }) => {
   return (
-    <Box sx={{ display: "flex", alignItems: "center", gap: 2, mt: 4, color: "white", bgcolor: "#171717", borderRadius: 1, p: 1 }}>
-      <FormControl variant="outlined" sx={{ minWidth: 120, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
-        <InputLabel sx={{ color: "white" }}>Категория</InputLabel>
-        <Select value={filters.category} onChange={onFilterChange("category")} label="Категория" sx={{ color: "white" }}>
-          <MenuItem value="">
-            <em>All</em>
-          </MenuItem>
-          {categoryList.map((category) => (
-            <MenuItem key={category} value={category}>
-              {category}
+    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: 2, mt: 4, color: "white", bgcolor: "#171717", borderRadius: 1, p: 1, flexWrap: "wrap", width: "100%" }}>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <FormControl variant="outlined" sx={{ minWidth: 85, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
+          <InputLabel sx={{ color: "white" }}>Категория</InputLabel>
+          <Select value={filters.category} onChange={onFilterChange("category")} label="Категория" sx={{ color: "white" }}>
+            <MenuItem value="">
+              <em>All</em>
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+            {categoryList.map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <FormControl variant="outlined" sx={{ minWidth: 120, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
-        <InputLabel sx={{ color: "white" }}>Price</InputLabel>
-        <Select value={filters.price} onChange={onFilterChange("price")} label="Цена" sx={{ color: "white" }}>
-          <MenuItem value="">
-            <em>No filter</em>
-          </MenuItem>
-          <MenuItem value="asc">Ascending</MenuItem>
-          <MenuItem value="desc">Descending</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl variant="outlined" sx={{ minWidth: 85, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
+          <InputLabel sx={{ color: "white" }}>Price</InputLabel>
+          <Select value={filters.price} onChange={onFilterChange("price")} label="Цена" sx={{ color: "white" }}>
+            <MenuItem value="">
+              <em>No filter</em>
+            </MenuItem>
+            <MenuItem value="asc">Ascending</MenuItem>
+            <MenuItem value="desc">Descending</MenuItem>
+          </Select>
+        </FormControl>
 
-      <FormControl variant="outlined" sx={{ minWidth: 120, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
-        <InputLabel sx={{ color: "white" }}>Name</InputLabel>
-        <Select value={filters.name} onChange={onFilterChange("name")} label="Название" sx={{ color: "white" }}>
-          <MenuItem value="">
-            <em>No filter</em>
-          </MenuItem>
-          <MenuItem value="asc">A-Z</MenuItem>
-          <MenuItem value="desc">Z-A</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl variant="outlined" sx={{ minWidth: 85, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
+          <InputLabel sx={{ color: "white" }}>Name</InputLabel>
+          <Select value={filters.name} onChange={onFilterChange("name")} label="Название" sx={{ color: "white" }}>
+            <MenuItem value="">
+              <em>No filter</em>
+            </MenuItem>
+            <MenuItem value="asc">A-Z</MenuItem>
+            <MenuItem value="desc">Z-A</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
 
-      <Typography>Page filter</Typography>
-      <FormControl variant="outlined" sx={{ minWidth: 120, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
-        <InputLabel sx={{ color: "white" }}>Price</InputLabel>
-        <Select value={pageFilter.price} onChange={onPageFilterChange("price")} label="Цена" sx={{ color: "white" }}>
-          <MenuItem value="">
-            <em>No filter</em>
-          </MenuItem>
-          <MenuItem value="asc">Ascending</MenuItem>
-          <MenuItem value="desc">Descending</MenuItem>
-        </Select>
-      </FormControl>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Typography>Page filter</Typography>
+        <FormControl variant="outlined" sx={{ minWidth: 85, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
+          <InputLabel sx={{ color: "white" }}>Price</InputLabel>
+          <Select value={pageFilter.price} onChange={onPageFilterChange("price")} label="Цена" sx={{ color: "white" }}>
+            <MenuItem value="">
+              <em>No filter</em>
+            </MenuItem>
+            <MenuItem value="asc">Ascending</MenuItem>
+            <MenuItem value="desc">Descending</MenuItem>
+          </Select>
+        </FormControl>
 
-      <FormControl variant="outlined" sx={{ minWidth: 120, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
-        <InputLabel sx={{ color: "white" }}>Name</InputLabel>
-        <Select value={pageFilter.name} onChange={onPageFilterChange("name")} label="Название" sx={{ color: "white" }}>
-          <MenuItem value="">
-            <em>No filter</em>
-          </MenuItem>
-          <MenuItem value="asc">A-Z</MenuItem>
-          <MenuItem value="desc">Z-A</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl variant="outlined" sx={{ minWidth: 85, border: "0.5px solid gray", borderRadius: 1, color: "white" }}>
+          <InputLabel sx={{ color: "white" }}>Name</InputLabel>
+          <Select value={pageFilter.name} onChange={onPageFilterChange("name")} label="Название" sx={{ color: "white" }}>
+            <MenuItem value="">
+              <em>No filter</em>
+            </MenuItem>
+            <MenuItem value="asc">A-Z</MenuItem>
+            <MenuItem value="desc">Z-A</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
     </Box>
   );
 };
--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -1,23 +1,36 @@
-import DropDown from "../dropDown/DropDown";
-import "./navigation.scss";
 import { Link } from "react-router-dom";
-
 import HomeIcon from "@mui/icons-material/Home";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-
 import LogoutIcon from "@mui/icons-material/Logout";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import AssignmentIndOutlinedIcon from "@mui/icons-material/AssignmentIndOutlined";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
-import { AppBar, Container, Typography } from "@mui/material";
+import { AppBar, Container, Typography, IconButton, Drawer, List, ListItem, ListItemIcon, ListItemText, Menu, MenuItem } from "@mui/material";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import { useGetProfileUserQuery } from "../../app/api/userApi";
 import Loader from "../Loader";
+import "./navigation.scss";
 
 const Navigation = () => {
   const { data: userInfo, isLoading } = useGetProfileUserQuery();
-  const [dropDown, setDropDown] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const toggleDrawer = (open: boolean) => {
+    setDrawerOpen(open);
+  };
+
+  const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar sx={{ backgroundColor: "#141414" }}>
       <Container>
@@ -27,44 +40,102 @@ const Navigation = () => {
               BELCOR
             </Typography>
           </Link>
-          <ul>
-            <li>
+
+          <List className="desktop-nav">
+            <ListItem>
               <Link to="/" data-text="Home">
                 <HomeIcon />
               </Link>
-            </li>
-            <li>
+            </ListItem>
+            <ListItem>
               <Link to="/profile/favorite" data-text="Favorite">
                 <FavoriteBorderOutlinedIcon />
               </Link>
-            </li>
-            <li>
+            </ListItem>
+            <ListItem>
               <Link to="/profile/basket" data-text="Basket">
                 <ShoppingCartOutlinedIcon />
               </Link>
-            </li>
+            </ListItem>
             <>{isLoading && <Loader />}</>
             {userInfo?.isAdmin && (
-              <li>
+              <ListItem>
                 <Link to="/profile/admin" data-text="Orders">
                   <AdminPanelSettingsIcon />
                 </Link>
-              </li>
+              </ListItem>
             )}
 
-            <li className="header__navigation-profile" onClick={() => setDropDown(!dropDown)}>
-              {dropDown && (
-                <DropDown
-                  items={[
-                    { label: `${userInfo?.username ? userInfo?.username : "Profile"}`, icon: <AccountBoxIcon />, href: "profile" },
-                    { label: `${userInfo?.username ? "Log Out" : "Log In"}`, icon: <LogoutIcon />, href: "logOut" },
-                  ]}
-                />
-              )}
+            <ListItem className="header__navigation-profile">
+              <IconButton onClick={handleProfileClick} color="inherit">
+                <AssignmentIndOutlinedIcon />
+              </IconButton>
+              <Menu anchorEl={anchorEl} open={open} onClose={handleMenuClose} anchorOrigin={{ vertical: "bottom", horizontal: "center" }} transformOrigin={{ vertical: "top", horizontal: "center" }}>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link to="/profile">
+                    <AccountBoxIcon sx={{ marginRight: "10px" }} />
+                    {userInfo?.username ? userInfo?.username : "Profile"}
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleMenuClose}>
+                  <Link to="/logOut">
+                    <LogoutIcon sx={{ marginRight: "10px" }} />
+                    {userInfo?.username ? "Log Out" : "Log In"}
+                  </Link>
+                </MenuItem>
+              </Menu>
+            </ListItem>
+          </List>
 
-              <AssignmentIndOutlinedIcon />
-            </li>
-          </ul>
+          <IconButton edge="end" color="inherit" aria-label="menu" onClick={() => toggleDrawer(true)} className="mobile-menu-button">
+            <MenuIcon />
+          </IconButton>
+          <Drawer anchor="right" open={drawerOpen} onClose={() => toggleDrawer(false)}>
+            <List>
+              <ListItem component={Link} to="/" onClick={() => toggleDrawer(false)}>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItem>
+              <ListItem component={Link} to="/profile/favorite" onClick={() => toggleDrawer(false)}>
+                <ListItemIcon>
+                  <FavoriteBorderOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary="Favorite" />
+              </ListItem>
+              <ListItem component={Link} to="/profile/basket" onClick={() => toggleDrawer(false)}>
+                <ListItemIcon>
+                  <ShoppingCartOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary="Basket" />
+              </ListItem>
+
+              {userInfo?.isAdmin && (
+                <ListItem component={Link} to="/profile/admin" onClick={() => toggleDrawer(false)}>
+                  <ListItemIcon>
+                    <AdminPanelSettingsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Orders" />
+                </ListItem>
+              )}
+            </List>
+
+            <List>
+              <ListItem component={Link} to="/profile" onClick={() => toggleDrawer(false)}>
+                <ListItemIcon>
+                  <AccountBoxIcon sx={{ marginRight: "10px" }} />
+                </ListItemIcon>
+                <ListItemText primary={userInfo?.username ? userInfo?.username : "Profile"} />
+              </ListItem>
+              <ListItem component={Link} to="/logOut" onClick={() => toggleDrawer(false)}>
+                <ListItemIcon>
+                  <LogoutIcon sx={{ marginRight: "10px" }} />
+                </ListItemIcon>
+                <ListItemText primary={userInfo?.username ? "Log Out" : "Log In"} />
+              </ListItem>
+            </List>
+          </Drawer>
         </nav>
       </Container>
     </AppBar>
