dd54494c0af768c8d24111022212a1754195e853
Shahzod <sibragimov999@gmail.com>
2024-09-27 18:11:58 +0500

    proxy-to-text3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/belcor': {
-          target: "https://belcor-seven.vercel.app" || "http://localhost:5001",
-          changeOrigin: true, 
-        }
-      }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/belcor/": "https://belcor-seven.vercel.app" || "http://localhost:5001",
     }
-  };
-});
+  }
+})
