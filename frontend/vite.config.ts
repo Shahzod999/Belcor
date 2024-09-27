cc8c743dc0bad2a1aa27085b916835d9ceb2a62a
Shahzod <sibragimov999@gmail.com>
2024-09-27 15:28:06 +0500

    .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/belcor/": "http://localhost:5001"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        [env.VITE_BASE_URL]: env.VITE_PROXY_URL || "http://localhost:5001"
+      }
     }
-  }
-})
+  };
+});
