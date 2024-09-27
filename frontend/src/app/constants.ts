b3241c7d371d984f39bc594441dba7b97259c5ab
Shahzod <sibragimov999@gmail.com>
2024-09-27 18:18:43 +0500

    urls

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_DUMMY_URL = import.meta.env.VITE_BASE_DUMMY_URL || 'https://dummyjson.com/';
-export const BASE_URL = import.meta.env.VITE_BASE_URL || '/belcor';
-export const USER_URL = import.meta.env.VITE_USER_URL || '/belcor/user';
-export const ORDERS_URL = import.meta.env.VITE_ORDERS_URL || '/belcor/products';
+export const BASE_DUMMY_URL = 'https://dummyjson.com/';
+export const BASE_URL = '/belcor';
+export const USER_URL = '/belcor/user';
+export const ORDERS_URL = '/belcor/products';
