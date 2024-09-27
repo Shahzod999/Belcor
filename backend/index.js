e22c1dc5b002b0f4e69761fc35c6cd5be96995be
Shahzod <sibragimov999@gmail.com>
2024-09-27 17:01:13 +0500

    cors-versel

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    // credentials: true,
+    credentials: true,
   })
 ); // снова забыл
 app.use(express.json());
